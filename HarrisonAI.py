1c004c35c28d962f33527e125eb6a332da4b71bd
amiresobh <42348899+amiresobh@users.noreply.github.com>
2023-05-01 15:42:44 +0330

    Update HarrisonAI.py

--- HarrisonAI.py
+++ HarrisonAI.py
@@ -3,7 +3,6 @@ import openai  # for calling the OpenAI API
 import pandas as pd  # for storing text and embeddings data
 import tiktoken  # for counting tokens
 from scipy import spatial  # for calculating vector similarities for search
-import dask.dataframe as dd
 import streamlit as st
 from streamlit_chat import message as st_message
 import requests
